67343943344c8ba5681572625d6c75e8dd46fe9c
arthur.eichmann <arthur@untermeitingen.net>
2023-07-04 23:09:06 +0200

    further progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-</h1>
+<div class="min-h-screen flex flex-col">
+    <slot />
+</div>
   
 <style lang="postcss">
     :global(html) {
