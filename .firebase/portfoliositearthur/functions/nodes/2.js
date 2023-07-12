fe5abf753774dc7e1d9a09b1e82c90da978f6197
Arthur Eichmann <arthur@untermeitingen.net>
2023-07-12 11:21:38 +0200

    delete firebase dir

--- .firebase/portfoliositearthur/functions/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/login/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.85126816.js","_app/immutable/chunks/scheduler.283841f9.js","_app/immutable/chunks/index.c9f7c5ae.js","_app/immutable/chunks/stores.d8ffc7f2.js","_app/immutable/chunks/singletons.91d2ce55.js","_app/immutable/chunks/index.145e5b94.js","_app/immutable/chunks/index.40b40403.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .firebase/portfoliositearthur/functions/nodes/4.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 4;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/login/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/4.865f04cb.js","_app/immutable/chunks/scheduler.283841f9.js","_app/immutable/chunks/index.c9f7c5ae.js","_app/immutable/chunks/firebase.2a115845.js","_app/immutable/chunks/index.145e5b94.js"];
-export const stylesheets = [];
-export const fonts = [];
